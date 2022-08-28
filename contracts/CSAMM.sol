b73799ff0404f74d983bd2fa56d77030f3d52462
Vincent <v3rdugo@yahoo.com>
2022-08-28 14:21:23 -0700

    add the OpenZeppelin ERC20 token interface

--- contracts/CSAMM.sol
+++ contracts/CSAMM.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "./IERC20.sol";
+
 contract CSAMM
 {
+    /**
+     * State Variables
+     */
+     IERC20 public immutable token0;
+     IERC20 public immutable token1;
+
     function swap() external
     {
 
@@ -15,6 +23,6 @@ contract CSAMM
 
     function removeLiquidity() external
     {
-        
+
     }
 }
